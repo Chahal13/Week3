e1195333d79d85be301d6b5e8b249e8a610f0dcf
Chahal13 <Chahal46sheridancollege.ca>
2020-01-23 09:30:05 -0500

    practice code

--- src/jaskaran/Jaskaran.java
+++ src/jaskaran/Jaskaran.java
@@ -16,7 +16,7 @@ public class Jaskaran {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("This is git hub");
+        System.out.println("This is a git hub");
     }
     
 }
